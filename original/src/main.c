e6c6edb0cdfd620ddcf1d96969437602ff62ad47
Luke Turnbull <your@email.com>
2026-04-12 16:46:28 +1000

    Create full screen mode

--- original/src/main.c
+++ original/src/main.c
@@ -19,6 +19,12 @@ bool initialize_window(void) {
     return false;
   };
 
+  SDL_DisplayMode display_mode;
+  SDL_GetCurrentDisplayMode(0, &display_mode);
+
+  window_width = display_mode.w;
+  window_height = display_mode.h;
+
   window =
       SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                        window_width, window_height, SDL_WINDOW_BORDERLESS);
